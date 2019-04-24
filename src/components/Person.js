9fda421161d990599b1a04b8b10b0be53d46d109
Jake Bills <jakebills1@gmail.com>
2019-04-24 12:02:49 -0600

    adds Person.js

---
+++ src/components/Person.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect} from 'react'
+import {Segment, Button, Icon, } from 'semantic-ui-react'
+import Species from './Species'
+import Planet from './Planet'
+import Starships from './Starships'
+
+const Person = ({ name, species, homeworld, starships, }) => {
+	const [showMore, setShowMore] = useState(false)
+	return (
+		<>
+			<Segment>
+				<div style={{display: "flex", justifyContent: "space-between"}}>
+					<div>{name}</div>
+					<Button icon color="green" onClick={() => setShowMore(!showMore)}>
+						<Icon name={showMore ? "minus" : "plus"} />
+					</Button>
+				</div>
+			</Segment>
+			{ showMore &&
+				<Segment.Group>
+					<Segment><Species url={species}/></Segment>
+					<Segment><Planet url={homeworld} /></Segment>
+					<Segment><Starships urlArr={starships}/></Segment>
+				</Segment.Group>
+			}
+		</>
+	)
+}
+export default Person;
