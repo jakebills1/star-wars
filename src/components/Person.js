b1569d6c89b5b1e49668b369c11eec57d3e6ae7d
Jake Bills <jakebills1@gmail.com>
2019-04-24 12:04:15 -0600

    removes warning

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react'
+import React, { useState, } from 'react'
 import {Segment, Button, Icon, } from 'semantic-ui-react'
 import Species from './Species'
 import Planet from './Planet'
